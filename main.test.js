b024c27d1833786d3efba3cca5adf6a0e79ca365
Anthony McMellen <anthony@BHB-CSET-18-13.local>
2018-12-07 09:22:47 -0500

    wrote basic js functionality for the game.

--- main.test.js
+++ main.test.js
@@ -1,3 +1,27 @@
-const rows = require('./rows')
+const {
+  addParagraph, addDiv
+} = require('./main');
 
+describe('addParagraph()', () => {
+  test('should append a paragraph to the body', () => {
+    let paragraphs = document.querySelectorAll('p');
+    expect(paragraphs.length).toBe(0);
 
+    const testText = 'Hello World';
+    addParagraph(testText);
+    paragraphs = document.querySelectorAll('p');
+    expect(paragraphs.length).toBe(1);
+    expect(paragraphs[0].innerHTML).toBe(testText);
+  });
+});
+
+describe('addDiv()', () => {
+  test('should append a div to the body', () => {
+    let div = document.querySelectorAll('div')
+    expect(div.length).toBe(0)
+
+    addDiv()
+    div = document.querySelectorAll('div')
+    expect(div.length).toBe(1)
+  });
+});
