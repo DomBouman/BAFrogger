16c71fab367bc4665fbbe451739a86cce933d935
Dominik Bouman <Decapause@icloud.com>
2018-12-09 23:38:00 -0500

    finished time limit

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ if (typeof module !== 'undefined') {
 };
 
 
- /* x: left side x position
+/* x: left side x position
  * y: top side y position
  * w: width of this Rectangle
  * h: height of this Rectangle
@@ -25,10 +25,10 @@ function Rectangle(x, y, w, h) {
 // Check intersection with any other Rectangle object.
 Rectangle.prototype.intersects = function(other) {
   return !(
-    this.x + this.w  <= other.x            ||
-    this.x           >= other.x + other.w  ||
-    this.y + this.h  <= other.y            ||
-    this.y           >= other.y + other.h
+    this.x + this.w <= other.x ||
+    this.x >= other.x + other.w ||
+    this.y + this.h <= other.y ||
+    this.y >= other.y + other.h
   );
 }
 
@@ -66,7 +66,7 @@ Frog.prototype.attach = function(other) {
 
 // Update the frog. If it is attached to an object, it will move with it.
 Frog.prototype.update = function() {
-  if(this.sitting_on !== null) {
+  if (this.sitting_on !== null) {
     this.x += this.sitting_on.speed;
   }
   this.x = constrain(this.x, 0, width - this.w);
@@ -78,7 +78,7 @@ Frog.prototype.show = function() {
   rect(this.x, this.y, this.w, this.h);
 }
 
- /* x: x position of the obstacle
+/* x: x position of the obstacle
  * y: y position of the obstacle
  * w: Obstacle width
  * h: Obstacle height
@@ -96,10 +96,10 @@ Obstacle.prototype = Object.create(Rectangle.prototype);
 // Move this obstacle by its speed, and wrap it if off the screen.
 Obstacle.prototype.update = function() {
   this.move(this.speed, 0);
-  if(this.x > width + grid_size) {
-    this.x = - this.w - grid_size;
+  if (this.x > width + grid_size) {
+    this.x = -this.w - grid_size;
   }
-  if(this.x < - this.w - grid_size) {
+  if (this.x < -this.w - grid_size) {
     this.x = width + grid_size;
   }
 }
@@ -114,7 +114,7 @@ function Row(y, count, speed, obs_width, spacing, offset, inverted) {
   Rectangle.call(this, 0, y, width, grid_size);
   this.obstacles = [];
   this.inverted = inverted;
-  for(let i = 0; i < count; i++) {
+  for (let i = 0; i < count; i++) {
     let x = i * spacing + offset;
     this.obstacles.push(new Obstacle(x, y, obs_width, grid_size, speed));
   }
@@ -125,14 +125,14 @@ Row.prototype = Object.create(Rectangle.prototype);
 
 // Shows this Row, showing all obstacles on it.
 Row.prototype.show = function() {
-  for(let i = 0; i < this.obstacles.length; i++) {
+  for (let i = 0; i < this.obstacles.length; i++) {
     this.obstacles[i].show();
   }
 }
 
 // Update all obstacles on this row.
 Row.prototype.update = function() {
-  for(let i = 0; i < this.obstacles.length; i++) {
+  for (let i = 0; i < this.obstacles.length; i++) {
     this.obstacles[i].update();
   }
 }
@@ -141,8 +141,8 @@ Row.prototype.update = function() {
 // Calculates which obstacle, if any, the collider has intersected.
 Row.prototype.hits = function(collider) {
   let obstacle = null;
-  for(let i = 0; i < this.obstacles.length; i++) {
-    if(collider.intersects(this.obstacles[i])) {
+  for (let i = 0; i < this.obstacles.length; i++) {
+    if (collider.intersects(this.obstacles[i])) {
       obstacle = this.obstacles[i];
     }
   }
@@ -167,16 +167,16 @@ function resetGame() {
 function setup() {
   width = 500;
   rows = [
-    new Row(            0, 1,    0,         width,   0,   0, true),
-    new Row(    grid_size, 1,    0,         width,   0,   0, true),
-    new Row(2 * grid_size, 2,  0.5, 4 * grid_size, 400,  10, true),
-    new Row(3 * grid_size, 3, -1.3, 2 * grid_size, 200,  30, true),
-    new Row(4 * grid_size, 2,  2.3, 3 * grid_size, 250,  25, true),
-    new Row(5 * grid_size, 1,    0,         width,   0,   0, true),
-    new Row(6 * grid_size, 3,  1.2, 1 * grid_size, 150, 100, false),
+    new Row(0, 1, 0, width, 0, 0, true),
+    new Row(grid_size, 1, 0, width, 0, 0, true),
+    new Row(2 * grid_size, 2, 0.5, 4 * grid_size, 400, 10, true),
+    new Row(3 * grid_size, 3, -1.3, 2 * grid_size, 200, 30, true),
+    new Row(4 * grid_size, 2, 2.3, 3 * grid_size, 250, 25, true),
+    new Row(5 * grid_size, 1, 0, width, 0, 0, true),
+    new Row(6 * grid_size, 3, 1.2, 1 * grid_size, 150, 100, false),
     new Row(7 * grid_size, 2, -3.5, 1 * grid_size, 200, 150, false),
-    new Row(8 * grid_size, 2,    2, 2 * grid_size, 300,   0, false),
-    new Row(9 * grid_size, 2,    0,         width,   0,   0, true),
+    new Row(8 * grid_size, 2, 2, 2 * grid_size, 300, 0, false),
+    new Row(9 * grid_size, 2, 0, width, 0, 0, true),
   ];
   createCanvas(width, rows.length * grid_size);
   resetGame();
@@ -192,12 +192,12 @@ function draw() {
 
   let intersects = null;
 
-  for(let i = 0; i < rows.length; i++) {
+  for (let i = 0; i < rows.length; i++) {
     rows[i].show();
     rows[i].update();
-    if(frog.intersects(rows[i])) {
+    if (frog.intersects(rows[i])) {
       intersects = rows[i].hits(frog);
-      if((intersects !== null) ^ rows[i].inverted) {
+      if ((intersects !== null) ^ rows[i].inverted) {
         resetGame();
       }
     }
@@ -214,37 +214,14 @@ function draw() {
 // p5js key pressed function, runs when any key is pressed on the keyboard
 // while the game is open.
 function keyPressed() {
-  if(keyCode === UP_ARROW) {
+  if (keyCode === UP_ARROW) {
     frog.move(0, -grid_size);
-  } else if(keyCode === DOWN_ARROW) {
+  } else if (keyCode === DOWN_ARROW) {
     frog.move(0, grid_size);
-  } else if(keyCode === LEFT_ARROW) {
+  } else if (keyCode === LEFT_ARROW) {
     frog.move(-grid_size, 0);
-  } else if(keyCode === RIGHT_ARROW) {
+  } else if (keyCode === RIGHT_ARROW) {
     frog.move(grid_size, 0);
   }
 };
 }
-
-function startTimer(duration, display) {
-    let timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-window.onload = function () {
-    let fiveMinutes = 60 * 1,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
